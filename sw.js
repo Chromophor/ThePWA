596f43d61cb2af289026a9b85b28af5695e56655
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2022-01-25 14:58:48 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,40 @@
 self.addEventListener("install", e => {
-   e.waitUntil(
-        caches.open("static").then(cache =>{
-            return cache.addAll([
-              "./", 
-              "./index.js", 
-              "./master.css", 
-              "./webmanifest.json",
-              "./Bilder/icon57.png",
-              "./Bilder/icon60.png",
-              "./Bilder/icon120.png",
-              "./Bilder/icon192.png",
-              "./Bilder/icon512.png"
-            ]);
-        })
-   );
+    e.waitUntil(
+         caches.open("static").then(cache =>{
+             return cache.addAll([
+               "./", 
+               "./index.js", 
+               "./master.css", 
+               "./webmanifest.json",
+               "./Bilder/icon57.png",
+               "./Bilder/icon60.png",
+               "./Bilder/icon120.png",
+               "./Bilder/icon192.png",
+               "./Bilder/icon512.png",
+             ]);
+         })
+    );
 });
+ 
+//NEW!!
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-      caches.match(event.request).then(response => {
-          return response || fetch(event.request);
-      })
+self.addEventListener('fetch', e => {
+  console.log(`Intercepting fetch request for: ${e.request.url}`);
+
+  e.respondWith(
+
+    caches.match(e.request).then(cachedResponse => cachedResponse || fetch(e.request))
   );
-
-  if (response) {
-    return response;
-  } else {
-    return fetch(event.request);
-  }
-
 });
 
-self.addEventListener("fetch", e => {
-    console.log(`Intercepting fetch request for: ${e.request.url}`);
-});
 
+//NEW!!
+self.addEventListener('push', event => {
+  event.waitUntil(
+    self.registration.showNotification('Sehen Sie sich Ihre Statistiken an!', {  
+      body: 'Sie waren die letzten Tage aktiv, hier sind Ihre Ergebnisse.',  
+      icon: '/Bilder/icon120.png',
+      tag: 'notification'
+    })
+  );
+});
