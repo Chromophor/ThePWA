d03a9eb6bb6d63c5f014ab3576ad34d6f2258bae
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2022-01-25 16:01:48 +0100

    Add files via upload

---
+++ sensor.js
@@ -0,0 +1,39 @@
+navigator.permissions.query({ name: 'accelerometer' })
+.then(result => {
+  if (result.state === 'denied') {
+    console.log('Permission to use accelerometer sensor is denied.');
+    return;
+  }
+    let acl = new Accelerometer({frequency: 60});
+    
+    acl.addEventListener('reading', () => {
+    console.log("Beschleunigung in X-Achse: " + acl.x);
+    console.log("Beschleunigung in Y-Achse: " + acl.y);
+    console.log("Beschleunigung in Z-Achse: " + acl.z);
+    });
+
+    acl.start();
+});
+
+
+
+navigator.permissions.query({ name: 'gyroscope' })
+.then(result => {
+  if (result.state === 'denied') {
+    console.log('Permission to use gyroscope sensor is denied.');
+    return;
+  }
+    let gyroscope = new Gyroscope({frequency: 60});
+
+    gyroscope.addEventListener('reading', e => {
+    console.log("Lage in X-Achse: " + gyroscope.x);
+    console.log("Lage in Y-Achse: " + gyroscope.y);
+    console.log("Lage in Z-Achse: " + gyroscope.z);
+    });
+    gyroscope.start();
+});
+
+
+
+
+
