b19d3e1d7124832d82cb6a7263d39c3597c2d122
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2022-01-26 18:11:11 +0100

    Add files via upload

--- sensor.js
+++ sensor.js
@@ -4,7 +4,7 @@ navigator.permissions.query({ name: 'accelerometer' })
     console.log('Permission to use accelerometer sensor is denied.');
     return;
   }
-    let acl = new Accelerometer({frequency: 60});
+    let acl = new Accelerometer({frequency: 5});
     
     acl.addEventListener('reading', () => {
     console.log("Beschleunigung in X-Achse: " + acl.x);
@@ -18,21 +18,6 @@ navigator.permissions.query({ name: 'accelerometer' })
 
     acl.start();
 });
-
-//HTML-Code!!
-
-<div id="BeschleunigungX">
-    NAN
-</div>
-
-<div id="BeschleunigungY">
-    NAN
-</div>
-
-<div id="BeschleunigungZ">
-    NAN
-</div>
-
 
 
 
@@ -55,53 +40,3 @@ navigator.permissions.query({ name: 'accelerometer' })
 });*/
 
 
-/*navigator.permissions.query({ name: 'accelerometer' }).then(result => {
-  if (result.state === 'denied') {
-      console.log('Permission to use accelerometer sensor is denied.');
-      return;
-  }
-
-  let acl = new Accelerometer({frequency: 30});
-  let max_magnitude = 0;
-  acl.addEventListener('activate', () => console.log('Ready to measure.'));
-  acl.addEventListener('error', error => console.log(`Error: ${error.name}`));
-  acl.addEventListener('reading', () => {
-      let magnitude = Math.hypot(acl.x, acl.y, acl.z);
-      if (magnitude > max_magnitude) {
-          max_magnitude = magnitude;
-          console.log(`Max magnitude: ${max_magnitude} m/s2`);
-      }
-  });
-  acl.start();
-});*/
-
-
-
-/*let accelerometer = null;
-try {
-  accelerometer = new Accelerometer({ frequency: 10 });
-  accelerometer.onerror = (event) => {
-    // Handle runtime errors.
-    if (event.error.name === 'NotAllowedError') {
-      console.log('Permission to access sensor was denied.');
-    } else if (event.error.name === 'NotReadableError') {
-      console.log('Cannot connect to the sensor.');
-    }
-  };
-  accelerometer.onreading = (e) => {
-    console.log(e);
-
-  };
-  accelerometer.start();
-} catch (error) {
-  // Handle construction errors.
-  if (error.name === 'SecurityError') {
-    console.log('Sensor construction was blocked by the Permissions Policy.');
-  } else if (error.name === 'ReferenceError') {
-    console.log('Sensor is not supported by the User Agent.');
-  } else {
-    throw error;
-  }
-}*/
-
-//HTML-code
