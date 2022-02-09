4e134af46add944b64982d9adf2e94b740cbfccd
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2022-02-09 13:47:06 +0100

    Add files via upload

--- sensor.js
+++ sensor.js
@@ -17,10 +17,10 @@ navigator.permissions.query({ name: 'accelerometer' })
     const BeschleunigungX = acl.x;
     const BeschleunigungY = acl.y;
     const BeschleunigungZ = acl.z;
-    var aGesamt = Math.sqrt(Math.pow(acl.x, 2) + Math.pow(acl.y, 2) + Math.pow((acl.z - 9.5), 2) );
+    var aGesamt = (Math.sqrt(Math.pow(acl.x, 2) + Math.pow(acl.y, 2) + Math.pow((acl.z), 2)) - 9.5);
     
     
-   // ((acl.x + acl.y + acl.z) - 9.5)
+    // ((acl.x + acl.y + acl.z) - 9.5)
     
 
     amaxX.innerHTML = "Gesamtbeschleunigung = " + aGesamt;
@@ -40,7 +40,7 @@ navigator.permissions.query({ name: 'accelerometer' })
       Schritte.innerHTML = "Anzahl der Schritte: " + anzahlSchritte;
     }
 
-    function resetA(){
+    function resetA() {
       aMax = 0;
       aMin = 0;
       aGesamt = 0;
@@ -49,15 +49,15 @@ navigator.permissions.query({ name: 'accelerometer' })
       aminX.innerHTML = "Minimale Beschleunigung: " + aMin;
       Schritte.innerHTML = "Anzahl der Schritte: " + anzahlSchritte;
     }
-  
-    });
+  });
 
-    acl.start();
+  acl.start();
 });
 
 
 
-//new
+
+/*
 if('geolocation' in navigator) {
 
 console.log("Geolokation verfügbar!");
@@ -67,9 +67,46 @@ navigator.geolocation.getCurrentPosition(function(position) {
   speed.innerHTML = "Aktuelle Geschwindigkeit: " + gesch;
   });
 }
+*/
 
 
+if (navigator.geolocation) {
+  var options = {
+    enableHighAccuracy: true,
+  }
+  navigator.geolocation.watchPosition(showPosition, showError, options);
+} else {
+  alert('Ihr Browser unterstützt die Geolocation API nicht.');
+}
 
+
+function showPosition(position) {
+  alert(
+      'Die geographische Position dieses Geräts ist (Stand: ' + new Date(position.timestamp).toLocaleTimeString() + '):\n'+
+      'Breitengrad: ' + position.coords.latitude + '° \n'+
+      'Längengrad: ' + position.coords.longitude + '° \n'+
+      '  Genauigkeit: ' + position.coords.accuracy + 'm\n' +
+      (position.coords.altitude ? ('Höhe: ' + position.coords.altitude + 'm\n' +
+                                  '  Genauigkeit: ' + position.coords.altitudeAccuracy + 'm') : "")
+  ) ;
+};
+
+function showError(error) {
+  switch(error.code) {
+      case error.PERMISSION_DENIED:
+          alert('Sie haben die Abfrage ihrer Position untersagt.');
+          break;
+      case error.POSITION_UNAVAILABLE:
+          alert('Es sind keine Positionsdaten verfügbar.');
+          break;
+      case error.TIMEOUT:
+          alert('Das Timeout für die Ortsanfrage wurde überschritten.');
+          break;
+      default:
+          alert('Es ist ein unbekannter Fehler aufgetreten (#' + error.code + ': ' + error.message + ')');
+          break;
+  }
+};
 
 
 /*
