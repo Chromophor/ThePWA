ec3901db05c70c9a7b348d648bb7239ff6a324c0
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2022-02-11 14:19:55 +0100

    Add files via upload

--- sensor.js
+++ sensor.js
@@ -17,7 +17,7 @@ navigator.permissions.query({ name: 'accelerometer' })
     const BeschleunigungX = acl.x;
     const BeschleunigungY = acl.y;
     const BeschleunigungZ = acl.z;
-    var aGesamt = Math.sqrt(Math.pow(acl.x, 2) + Math.pow(acl.y, 2) + Math.pow(acl.z, 2) + Math.pow(9.5 , 2));
+    var aGesamt = Math.sqrt(Math.pow((Math.pow(acl.x, 2) + Math.pow(acl.y, 2) + Math.pow(acl.z, 2) - 9.5), 2));
     //var aGesamt = (Math.sqrt(Math.pow(acl.x, 2) + Math.pow(acl.y, 2) + Math.pow((acl.z), 2)) - 9.5);
     
     
@@ -87,7 +87,7 @@ function showPosition(position) {
       'Breitengrad: ' + position.coords.latitude + '° \n'+
       'Längengrad: ' + position.coords.longitude + '° \n'+
       'Geschwindigkeit: ' + position.coords.speed + 'm\s'
-  ) ;
+  );
 };
 
 function showError(error) {
