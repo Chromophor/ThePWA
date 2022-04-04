5c3a9671b50c505771757f73671c31cc9bf03ed5
TheMoonlandingV2 <88386049+TheMoonlandingV2@users.noreply.github.com>
2022-04-04 14:08:17 +0200

    Add files via upload

--- sensor.js
+++ sensor.js
@@ -36,7 +36,7 @@ navigator.permissions.query({ name: 'accelerometer' })
       anzahlSchritte = anzahlSchritte + 1;
       localStorage.setItem("Schritte", anzahlSchritte);
       ladeBalken();
-      anzahlSchritte.innerHTML = anzahlSchritte;
+      Anzahlschritte.innerHTML = anzahlSchritte;
     }
   });
 
